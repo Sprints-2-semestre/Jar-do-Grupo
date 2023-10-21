19057e38f3b4c0ac3bbac993f9f6fc396a9e9fa8
DiegoHessel <diego.verdi@sptech.school>
2023-10-21 01:41:48 -0300

    Script finalizado java e jar

--- script_farmacos.sh
+++ script_farmacos.sh
@@ -3,21 +3,17 @@
 VERSAO=17
 echo -e "FÁRMACOS"
 echo -e "Olá usuário, serei seu assistente para instalação do Java!"
+sleep 1
 echo -e "Verificando se você possui o Java instalado na sua máquina!"
+sleep 1
 
 # Verifica se o Java está instalado
 if java -version &> /dev/null; then
     echo -e "Você já possui o Java instalado na sua máquina!"
-    
-   
-echo -e "Vamos atualizar os pacotes..."
+    echo -e "Vamos atualizar os pacotes..."
+    sleep 1
     sudo apt update && sudo apt upgrade -y
-    clear
-
-    sudo apt update && sudo apt upgrade
-
-    sudo apt
-echo -e "Pacotes atualizados!"
+    echo -e "Pacotes atualizados!"
 else
     echo -e "Não foi encontrada nenhuma versão do Java na sua máquina, mas iremos resolver isso!"
     echo -e "Você deseja instalar o Java na sua máquina (S/N)?"
@@ -27,25 +23,30 @@ else
         echo -e "Ok! Você decidiu instalar o Java na sua máquina."
 
         # Instala o Java
-        sudo apt install openjdk-17-jre y
-        
+        sudo apt install openjdk-$VERSAO-jre
         echo -e "Java instalado com sucesso!"
     else
         echo -e "Entendido, você optou por não instalar o Java. Até a próxima!"
-	exit 1
+        exit 1
     fi
-
-mkdir dataxtract
-cd dataxtract
-git clone "https://github.com/Sprints-2-semestre/Jar-do-Grupo.git"
-find . -name "*.jar" -exec chmod +x {} \;
-sudo java -jar dataxtract.jar
 fi
-mkdir dataxtract
-cd dataxtract
-git clone "https://github.com/Sprints-2-semestre/Jar-do-Grupo.git"
 
-cd Jar-do-Grupo\looca-oficial\target\
+curl -LJO https://github.com/Sprints-2-semestre/Jar-do-Grupo/raw/main/looca-oficial/target/looca-oficial-1.0-jar-with-dependencies.jar
 
-find . -name "*.jar" -exec chmod +x {} \;
-sudo java -jar looca-oficial-1.0-jar-with-dependencies.jar
+if [ $? -eq 0 ]; then
+    # Verificando se o arquivo baixado é um arquivo .jar válido
+    if [ -f looca-oficial-1.0-jar-with-dependencies.jar ]; then
+        echo ""
+        echo "Iniciando o software"
+        sleep 1
+        echo "Bem-Vindo a Fármacos"
+        echo ""
+        java -jar looca-oficial-1.0-jar-with-dependencies.jar
+    else
+        echo "Erro ao rodar o .jar"
+        exit 1
+    fi
+else
+    echo "Erro ao executar o curl"
+    exit 1
+fi
